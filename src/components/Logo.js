11a79671d2aa7b568aae1b882aca5da3236a0b86
Nixikaz <nixikaz@gmail.com>
2019-03-15 15:50:55 -0700

    Added back ground grid and more BEM for css

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Logo = props => {
   return (
     <div className="logo">
-      <h2>Kevin Zembrodt</h2>
+      <h1 className="logo--light">
+        Kev<span className="logo--opacity">in</span>
+      </h1>
+      <h1 className="logo__lastname">
+        Zem<span className="logo--opacity">brodt</span>
+      </h1>
     </div>
   );
 };
--- src/components/SkillsCard.js
+++ src/components/SkillsCard.js
@@ -4,10 +4,16 @@ export default class SkillsCard extends Component {
   render() {
     const { title, img, content } = this.props;
     return (
-      <section>
-        <h1>{title}</h1>
-        <img className="svg--large" src={img} alt={title} />
-        <p>{content}</p>
+      <section className="card">
+        <div className="card__title">
+          <h1>{title}</h1>
+        </div>
+        <div className="card__body">
+          <p>
+            <img className="svg--large" src={img} alt={title} />
+            {content}
+          </p>
+        </div>
       </section>
     );
   }
