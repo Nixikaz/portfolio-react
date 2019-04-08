3e6054a359aeb5e577bde674fc60c2bedd10b485
Kevin Zembrodt <nixikaz@gmail.com>
2019-04-08 13:13:12 -0700

    added forms on Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,6 +12,12 @@ export default class Contact extends Component {
             <form action="/thankyou" netlify>
               <label htmlFor="fName">First Name</label>
               <input type="text" name="fName" id="firstName" />
+              <label htmlFor="lName">Last Name</label>
+              <input type="text" name="lName" id="lastName" />
+              <label htmlFor="email">E-Mail Address</label>
+              <input type="text" name="email" id="emailAddress" />
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="shortMessage" cols="30" rows="10"></textarea>
             </form>
           </div>
         </section>
