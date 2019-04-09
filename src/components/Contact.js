3c41acea74815e061a9da87bb3adf26eef911b25
Nixikaz <nixikaz@gmail.com>
2019-04-08 21:07:49 -0700

    updated responsive grid, centered contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Contact extends Component {
   render() {
     return (
-      <main className="content">
+      <main className="content content--single">
         <section className="card">
           <div className="card__title">
             <h1>Contact</h1>
@@ -17,7 +17,7 @@ export default class Contact extends Component {
               <label htmlFor="email">E-Mail Address</label>
               <input type="text" name="email" id="emailAddress" />
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="shortMessage" cols="30" rows="10"></textarea>
+              <textarea name="message" id="shortMessage" cols="30" rows="10" />
             </form>
           </div>
         </section>
