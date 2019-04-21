dde667ede8ae34c47ca96eea6e9bbd9b427cb131
Nixikaz <nixikaz@gmail.com>
2019-04-20 19:15:25 -0700

    centering lists in card

--- src/components/Css.js
+++ src/components/Css.js
@@ -9,15 +9,23 @@ const Css = () => {
         <h1>CSS</h1>
       </div>
       <div className="card__body">
-      <ul className="card__list">
-        <li><abbr title="Cascading Style Sheets">CSS</abbr>3</li>
-        <li>Flexbox</li>
-        <li>Grid</li>
-        <li><abbr title="Block, Element, Modifier">BEM</abbr></li>
-        <li><em>Bootstrap</em></li>
-        <li><em>Materialize</em></li>
-        <li>Responsive Design</li>
-      </ul>
+        <ul className="card__list">
+          <li>
+            <abbr title="Cascading Style Sheets">CSS</abbr>3
+          </li>
+          <li>Flexbox</li>
+          <li>Grid</li>
+          <li>
+            <abbr title="Block, Element, Modifier">BEM</abbr>
+          </li>
+          <li>
+            <em>Bootstrap</em>
+          </li>
+          <li>
+            <em>Materialize</em>
+          </li>
+          <li>Responsive Design</li>
+        </ul>
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,36 +6,25 @@ import { Spring } from 'react-spring/renderprops';
 
 export default class Home extends Component {
   render() {
-    const animateFrom = {opacity: 0, transform: 'translateX(200px)'};
-    const animateTo = {opacity: 1, transform: 'translateX(0px)'};
+    const animateFrom = { opacity: 0, transform: 'translate(-10px, 20px)' };
+    const animateTo = { opacity: 1, transform: 'translate(0, 0)' };
     return (
       <main className="content">
-        <Spring
-          from={ animateFrom }
-          to={ animateTo }
-          config={{ delay: 500, duration: 300, tension: 100, friction: 14}}
-        >
+        <Spring from={animateFrom} to={animateTo} config={{ delay: 500, duration: 300, tension: 100, friction: 14 }}>
           {props => (
             <section className="card__section" style={props}>
               <Html />
             </section>
           )}
         </Spring>
-        <Spring
-          from={ animateFrom }
-          to={ animateTo }
-          config={{ delay: 1000, duration: 300 }}
-        >
+        <Spring from={animateFrom} to={animateTo} config={{ delay: 1000, duration: 300 }}>
           {props => (
             <section className="card__section" style={props}>
               <Css />
             </section>
           )}
         </Spring>
-        <Spring
-          from={ animateFrom }
-          to={ animateTo }
-          config={{ delay: 1500, duration: 300}}>
+        <Spring from={animateFrom} to={animateTo} config={{ delay: 1500, duration: 300 }}>
           {props => (
             <section className="card__section" style={props}>
               <Javascript />
