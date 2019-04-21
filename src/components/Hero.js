e6295bdc3564a3e44e2d1718dcee8772b89370bb
Nixikaz <nixikaz@gmail.com>
2019-04-20 21:02:22 -0700

    completed home and animations

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { Spring, config } from 'react-spring/renderprops';
 import '../images/code_hero.jpg';
 
 export default class Hero extends Component {
   render() {
     return (
-      <div className="hero">
-        <div className="hero__grid">
-          <span className="hero__intro">Hi, my name is</span>
-          <span className="hero__firstname">Kevin</span>
-          <span className="hero__and">and I am a</span>
-          <span className="hero__lastname">Zembrodt</span>
-          <span className="hero__webdev">front-end developer</span>
-        </div>
-      </div>
+      <Spring from={{ height: 800, opacity: 0 }} to={{ height: 250, opacity: 1 }} config={config.gentle}>
+        {({ height, opacity }) => (
+          <div className="hero" style={{ height }}>
+            <div className="hero__grid" style={{ opacity }}>
+              <span className="hero__intro">Hi, my name is</span>
+              <span className="hero__firstname">Kevin</span>
+              <span className="hero__and">and I am a</span>
+              <span className="hero__lastname">Zembrodt</span>
+              <span className="hero__webdev">front-end developer</span>
+            </div>
+          </div>
+        )}
+      </Spring>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,25 +6,25 @@ import { Spring } from 'react-spring/renderprops';
 
 export default class Home extends Component {
   render() {
-    const animateFrom = { opacity: 0, transform: 'translate(-10px, 20px)' };
-    const animateTo = { opacity: 1, transform: 'translate(0, 0)' };
+    const animateFrom = { opacity: 0, transform: 'translateY(20px)' };
+    const animateTo = { opacity: 1, transform: 'translateY(0)' };
     return (
       <main className="content">
-        <Spring from={animateFrom} to={animateTo} config={{ delay: 500, duration: 300, tension: 100, friction: 14 }}>
+        <Spring from={animateFrom} to={animateTo} config={{ delay: 1300, duration: 300, tension: 100, friction: 14 }}>
           {props => (
             <section className="card__section" style={props}>
               <Html />
             </section>
           )}
         </Spring>
-        <Spring from={animateFrom} to={animateTo} config={{ delay: 1000, duration: 300 }}>
+        <Spring from={animateFrom} to={animateTo} config={{ delay: 1600, duration: 300 }}>
           {props => (
             <section className="card__section" style={props}>
               <Css />
             </section>
           )}
         </Spring>
-        <Spring from={animateFrom} to={animateTo} config={{ delay: 1500, duration: 300 }}>
+        <Spring from={animateFrom} to={animateTo} config={{ delay: 1900, duration: 300 }}>
           {props => (
             <section className="card__section" style={props}>
               <Javascript />
