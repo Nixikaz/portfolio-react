d64f55fee14cce34577ec40d6db4f2f8dd568713
Kevin Zembrodt <nixikaz@gmail.com>
2019-04-29 13:25:42 -0700

    added descriptions to projects rps and etch

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,11 +13,11 @@ export default class Hero extends Component {
         {({ height, opacity }) => (
           <div className="hero" style={{ height }}>
             <div className="hero__grid" style={{ opacity }}>
-              <span className="hero__intro">Hi, my name is</span>
+              {/* <span className="hero__intro">Hi, my name is</span>
               <span className="hero__firstname">Kevin</span>
               <span className="hero__and">and I am a</span>
               <span className="hero__lastname">Zembrodt</span>
-              <span className="hero__webdev">front-end developer</span>
+              <span className="hero__webdev">front-end developer</span> */}
             </div>
           </div>
         )}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,16 +8,14 @@ const Projects = () => {
       <section id="projects">
         <h1>Projects</h1>
         <p>
-          <img src={Rps} alt="Rock, Paper, Scissors" id="rps_img" />
-          What is going on with this. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nisi voluptas
-          beatae, reprehenderit obcaecati quae quo nesciunt voluptatem iure. Accusantium, laboriosam. Expedita maxime
-          tempora provident dicta unde, commodi tenetur illo!
+          <a href="https://nixikaz.github.io/" target="_blank" rel="noopener noreferrer"><img src={Rps} alt="Rock, Paper, Scissors" id="rps_img" /></a>
+          This was an early project that was done entirely in vanilla JavaScript. The game is simple but shows off my knowledge
+          of DOM creation and conditional operations. <br />The source code for this project can be found on my <a href="https://github.com/Nixikaz/Nixikaz.github.io" target="_blank" rel="noopener noreferrer">GitHub</a>.
         </p>
         <p>
-          <img src={Etch} alt="Rock, Paper, Scissors" id="etch_img" />
-          What is going on with this. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nisi voluptas
-          beatae, reprehenderit obcaecati quae quo nesciunt voluptatem iure. Accusantium, laboriosam. Expedita maxime
-          tempora provident dicta unde, commodi tenetur illo!
+          <a href="https://nixikaz.github.io/etch/etchasketch.html" target="_blank" rel="noopener noreferrer"><img src={Etch} alt="Rock, Paper, Scissors" id="etch_img" /></a>
+          This project was an early one as well. Again, it uses vanilla JavaScript and shows off DOM creation, looping, and conditionals.<br />
+          The source code for this project is on my <a href="https://github.com/Nixikaz/Nixikaz.github.io/tree/master/etch" target="_blank" rel="noopener noreferrer">GitHub</a>.
         </p>
       </section>
     </main>
