f29b6b7b95786b32ec7c1d895de0b00fa254245a
Nixikaz <nixikaz@gmail.com>
2019-04-29 21:16:56 -0700

    added projects and formating

--- README.md
+++ README.md
@@ -2,6 +2,12 @@ This repository is for my portfolio website. [KevZem.com](https://www.kevzem.com
 
 # About
 
-This website was created using [React](https://github.com/facebook/react/) and [Create React app](https://github.com/facebook/create-react-app), The only other library that is used is [sanitize.css](https://github.com/csstools/sanitize.css)
+This website was created using the following:
+
+- [React](https://github.com/facebook/react/)
+- [React-Router](https://github.com/ReactTraining/react-router)
+- [Create React app](https://github.com/facebook/create-react-app)
+- [React-Spring](https://github.com/react-spring/react-spring)
+- [sanitize.css](https://github.com/csstools/sanitize.css)
 
 All other code was written by me.
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Portfolio from '../images/portfolio.png';
 import Rps from '../images/rps.png';
 import Etch from '../images/etch.png';
 
@@ -7,16 +8,52 @@ const Projects = () => {
     <main className="content content--single">
       <section id="projects">
         <h1>Projects</h1>
-        <p>
-          <a href="https://nixikaz.github.io/" target="_blank" rel="noopener noreferrer"><img src={Rps} alt="Rock, Paper, Scissors" id="rps_img" /></a>
-          This was an early project that was done entirely in vanilla JavaScript. The game is simple but shows off my knowledge
-          of DOM creation and conditional operations. <br />The source code for this project can be found on my <a href="https://github.com/Nixikaz/Nixikaz.github.io" target="_blank" rel="noopener noreferrer">GitHub</a>.
-        </p>
-        <p>
-          <a href="https://nixikaz.github.io/etch/etchasketch.html" target="_blank" rel="noopener noreferrer"><img src={Etch} alt="Rock, Paper, Scissors" id="etch_img" /></a>
-          This project was an early one as well. Again, it uses vanilla JavaScript and shows off DOM creation, looping, and conditionals.<br />
-          The source code for this project is on my <a href="https://github.com/Nixikaz/Nixikaz.github.io/tree/master/etch" target="_blank" rel="noopener noreferrer">GitHub</a>.
-        </p>
+        <div>
+          <h2>My Portfolio</h2>
+          <p>
+            <a href="https://www.kevzem.com" target="_blank" rel="noopener noreferrer">
+              <img src={Portfolio} alt="Portfolio" id="portfolio_img" />
+            </a>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolorem, quidem obcaecati fugiat eaque nemo
+            doloribus alias ratione sint. Fugit voluptas soluta officia temporibus eaque reiciendis nihil facilis
+            dolorum nisi!
+            <br />
+            <a href="https://github.com/Nixikaz/portfolio-react" target="_blank" rel="noopener noreferrer">
+              Source Code
+            </a>
+          </p>
+        </div>
+        <div>
+          <h2>Rock, Paper, Scissors</h2>
+          <p>
+            <a href="https://nixikaz.github.io/" target="_blank" rel="noopener noreferrer">
+              <img src={Rps} alt="Rock, Paper, Scissors" id="rps_img" />
+            </a>
+            This was an early project that was done entirely in vanilla JavaScript. The game is simple but shows off my
+            knowledge of DOM creation and conditional statements. <br />
+            <a href="https://github.com/Nixikaz/Nixikaz.github.io" target="_blank" rel="noopener noreferrer">
+              Source Code
+            </a>
+          </p>
+        </div>
+        <div>
+          <h2>Etch-a-Sketch</h2>
+          <p>
+            <a href="https://nixikaz.github.io/etch/etchasketch.html" target="_blank" rel="noopener noreferrer">
+              <img src={Etch} alt="Rock, Paper, Scissors" id="etch_img" />
+            </a>
+            This project was an early one as well. Again, it uses vanilla JavaScript and shows off DOM creation,
+            looping, and conditional statements.
+            <br />
+            <a
+              href="https://github.com/Nixikaz/Nixikaz.github.io/tree/master/etch"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Source Code
+            </a>
+          </p>
+        </div>
       </section>
     </main>
   );
